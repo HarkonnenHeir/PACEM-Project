4630542f5c718f1926c1c60c63d534783ac5b38e
Ambition <150349240+HarkonnenHeir@users.noreply.github.com>
2024-12-08 20:31:17 +0100

    Update code arduino.ino
    Structure de base appréhendée

--- code arduino.ino
+++ code arduino.ino
@@ -3,6 +3,8 @@
 #define LED_NUMBER 270
 #define LED_PIN 50
 
+String str = "None";
+
 CRGB leds[LED_NUMBER];
 
 void setup() {
@@ -20,3 +22,24 @@ void loop() {
 
 void vocal_recognition() {
 }
+
+void pacem_starts() {
+}
+
+void pause() {
+}
+
+void create_immersion(description) {
+  if ("combat") in description
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(255, 0, 0); // Rouge pur (255 pour rouge, 0 pour vert et bleu)
+  }
+  
+  // Afficher les couleurs sur les LEDs
+  FastLED.show();
+}
+}
+// À effectuer dans le code :
+// L'éclairage se met en pause quand le bouton Play/Pause est pressé
+// Au démarrage du PACEM, le bandeau émet une lumière blanche
+// Le code associe des mots à une ambiance

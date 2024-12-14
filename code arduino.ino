c658061c4959f359511a4f4b1d4349badbf1613a
Ambition <150349240+HarkonnenHeir@users.noreply.github.com>
2024-12-14 20:09:06 +0100

    Update code arduino.ino
    Révision totale du code

--- code arduino.ino
+++ code arduino.ino
@@ -7,15 +7,23 @@ int PreviousRed = 0;
 int PreviousGreen = 0;
 int PreviousBlue = 0;
 
+byte heat[NUM_LEDS];
+
 CRGB leds[NUM_LEDS]; 
 
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
-  set_color(100, 100, 100);
-  delay(3000);
-  set_color(0, 0, 0);
+  for (int i = 0; i < 250; i++) {
+    set_color(i, i, i);
+    delay(5);
+  }
+  for (int i = 0; i < 250; i++) {
+    set_color(250-i, 250-i, 250-i);
+    delay(5);
+  }
+  
 }
 
 void loop() {
@@ -31,10 +39,45 @@ void loop() {
     }
 
     if (description.indexOf("rivière") >= 0) {
-      set_color(0, 0, 255);
+      set_color(40, 147, 250);
     }
-    else if (description.indexOf("forêt") >= 0) {
-      set_color(0, 255, 0);
+    if (description.indexOf("forêt") >= 0) {
+      set_color(40, 122, 18);
+    }
+    if (description.indexOf("montagne") >= 0) {
+      set_color(222, 148, 18);
+    }
+    if (description.indexOf("laboratoire") >= 0) {
+      set_color(200, 18, 222);
+    }
+    if (description.indexOf("océan") >= 0) {
+      set_color(0, 74, 231);
+    }
+    if (description.indexOf("enfer") >= 0) {
+      set_color(228, 86, 0);
+    }
+    if (description.indexOf("église") >= 0) {
+      set_color(255, 250, 155);
+    }
+    if (description.indexOf("jungle") >= 0) {
+      set_color(137, 192, 139);
+    }
+    if (description.indexOf("plaine") >= 0) {
+      set_color(213, 225, 22);
+    }
+    if (description.indexOf("feu") >= 0) {
+      simulate_campfire();
+    }
+    if (description.indexOf("prison") >= 0) {
+      set_color(50, 50, 50);
+    }
+    if (description.indexOf("nuit") >= 0) {
+      
+    }
+    if (description.indexOf("combat") >= 0) {
+      set_color(250, 0, 0);
+    }
+    if (description.indexOf("taverne") >= 0) {
     }
     
   }
@@ -47,7 +90,7 @@ void set_color(uint8_t red, uint8_t green, uint8_t blue) {
   PreviousBlue = blue;
 
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(PreviousRed, PreviousGreen, PreviousBlue);
+    leds[i] = CRGB(red, green, blue);
   }
   FastLED.show();
 }
@@ -65,3 +108,8 @@ void play() {
   }
   FastLED.show();
 }
+
+void simulate_campfire() {
+  
+}
+
